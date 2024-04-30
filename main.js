54af296ac9532c7b150a359efa5f13c02a557fb8
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2024-04-30 23:23:00 +0200

    CSS advances

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-document.getElementById('search-button').addEventListener('click', function() {
-  var input = document.getElementById('search-input').value;
-  var option = document.getElementById('search-options').value;
+// document.getElementById('search-button').addEventListener('click', function() {
+//   var input = document.getElementById('search-input').value;
+//   var option = document.getElementById('search-options').value;
 
-  var endpoint;
-  switch(option) {
-      case 'Nombre de plato':
-          endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
-          break;
-      case 'Categoría':
-          endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${input}`;
-          break;
-      case 'Ingrediente principal':
-          endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${input}`;
-          break;
-      case 'Área':
-          endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${input}`;
-          break;
-  }
+//   var endpoint;
+//   switch(option) {
+//       case 'Nombre de plato':
+//           endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
+//           break;
+//       case 'Categoría':
+//           endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${input}`;
+//           break;
+//       case 'Ingrediente principal':
+//           endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${input}`;
+//           break;
+//       case 'Área':
+//           endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${input}`;
+//           break;
+//   }
 
-  fetch(endpoint)
-      .then(response => response.json())
-      .then(data => {
-          if (data.meals === null) {
-              alert('No se encontraron resultados. Por favor, intente con otra búsqueda.');
-          } else {
-              console.log(data.meals);
-          }
-      });
-});
+//   fetch(endpoint)
+//       .then(response => response.json())
+//       .then(data => {
+//           if (data.meals === null) {
+//               alert('No se encontraron resultados. Por favor, intente con otra búsqueda.');
+//           } else {
+//               console.log(data.meals);
+//           }
+//       });
+// });
